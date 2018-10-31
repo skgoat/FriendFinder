4bd19cf387ed7419672dd5943d7fdd869aaabb5e
skgoat <kellysp11@gmail.com>
2018-10-31 18:03:48 -0400

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendsdata = require("../data/friends");;
+var friends = require("../data/friends");;
 
 
 // ===============================================================================
@@ -19,7 +19,7 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friendsdata);
+    res.json(friends);
   });
 
 
@@ -32,32 +32,63 @@ module.exports = function(app) {
   // Then the server saves the data to the friendsdata array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    var newscores = req.body.scores;
-    var scores
-    
-    if (friendsdata.length < 5) {
-      friendsdata.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+  	app.post('/api/friends', function(req, res){
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+		// Note the code here. Our "server" will respond to a user's survey result
+		// Then compare those results against every user in the database.
+		// It will then calculate the difference between each of the numbers and the user's numbers.
+		// It will then choose the user with the least differences as the "best friend match."
+		// In the case of multiple users with the same result it will choose the first match.
+		// After the test, it will push the user to the database. 
 
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    friendsdata.length = [];
-   
+		// We will use this object to hold the "best match". We will constantly update it as we 
+		// loop through all of the options 
+		var bestMatch = {
+			name: "",
+			photo: "",
+			friendDifference: 1000
+		};
 
-    res.json({ ok: true });
-  });
-};
+		// Here we take the result of the user's survey POST and parse it.
+		var userData 	= req.body;
+		var userName 	= userData.name;
+		var userPhoto 	= userData.photo;
+		var userScores 	= userData.scores;
+
+		// This variable will calculate the difference between the user's scores and the scores of
+		// each user in the database
+		var totalDifference = 0;
+
+		// Here we loop through all the friend possibilities in the database. 
+		for  (var i=0; i< friends.length; i++) {
+
+		console.log(friends[i].name);
+		totalDifference = 0;
+
+		// We then loop through all the scores of each friend
+	for (var j=0; j< friends[i].scores[j]; j++){
+
+				// We calculate the difference between the scores and sum them into the totalDifference
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+				// If the sum of differences is less then the differences of the current "best match"
+				if (totalDifference <= bestMatch.friendDifference){
+
+					// Reset the bestMatch to be the new friend. 
+					bestMatch.name = friends[i].name;
+					bestMatch.photo = friends[i].photo;
+					bestMatch.friendDifference = totalDifference;
+				}
+			}
+		}
+
+		// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+		// the database will always return that the user is the user's best friend).
+	friends.push(userData);
+
+		// Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
+	res.json(bestMatch);
+
+	});
+
+}
