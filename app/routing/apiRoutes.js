a051490452da192d33e2c77643afc84b9ac61d50
skgoat <kellysp11@gmail.com>
2018-10-31 21:33:02 -0400

    clean up

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -49,32 +49,27 @@ module.exports = function(app) {
 			friendDifference: 1000
 		};
 
-		// Here we take the result of the user's survey POST and parse it.
 		var userData 	= req.body;
 		var userName 	= userData.name;
 		var userPhoto 	= userData.photo;
 		var userScores 	= userData.scores;
 
-		// This variable will calculate the difference between the user's scores and the scores of
-		// each user in the database
 		var totalDifference = 0;
 
-		// Here we loop through all the friend possibilities in the database. 
+	
 		for  (var i=0; i< friends.length; i++) {
 
 		console.log(friends[i].name);
 		totalDifference = 0;
 
-		// We then loop through all the scores of each friend
 	for (var j=0; j< friends[i].scores[j]; j++){
 
-				// We calculate the difference between the scores and sum them into the totalDifference
+				
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
-				// If the sum of differences is less then the differences of the current "best match"
+				
 				if (totalDifference <= bestMatch.friendDifference){
 
-					// Reset the bestMatch to be the new friend. 
 					bestMatch.name = friends[i].name;
 					bestMatch.photo = friends[i].photo;
 					bestMatch.friendDifference = totalDifference;
@@ -82,11 +77,8 @@ module.exports = function(app) {
 			}
 		}
 
-		// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-		// the database will always return that the user is the user's best friend).
 	friends.push(userData);
-
-		// Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
+ 
 	res.json(bestMatch);
 
 	});
